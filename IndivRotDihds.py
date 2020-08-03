86b3bc1a42fe41a3daf40e9d80ad36f3e8eb3e9b
rduke199 <50717504+rduke199@users.noreply.github.com>
2020-08-03 15:29:46 -0400

    Add files via upload

---
+++ IndivRotDihds.py
@@ -0,0 +1,161 @@
+import os
+import re
+import numpy as np
+import subprocess
+import shutil
+
+import pymatgen
+from pymatgen.core import Molecule
+from pymatgen.io.gaussian import GaussianInput
+
+import ocelot
+from ocelot.routines.conformerparser import pmgmol_to_rdmol
+from ocelot.task.wtuning import WtuningJob
+from ocelot.task.confgen import ConfGen
+
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.rdMolTransforms import SetDihedralDeg
+from rdkit.Chem.rdmolfiles import MolToXYZBlock
+
+
+def MakesGauInputs(mol_file,dihed_atoms_file,w,num_conform=19):
+    """
+    Creates Gaussian input files in its own folder for mol_file structues that
+    freezes the central angle at iterations of 180/num_conform degrees. This
+    function requires Gaussian output file of molecule in question and file
+    with dihedral atoms to be in the current working directory.
+    """
+
+    molecule_name = mol_file.split('/')[-1][:-12]
+    mol_pymat = Molecule.from_file(mol_file)
+    molecule = pmgmol_to_rdmol(mol_pymat)[0]
+
+    data = open(dihed_atoms_file,'r').readlines()
+    dihedral1 = data[0].split(',')[0]
+    dihedral2 = data[0].split(',')[1]
+
+    d1atom1_num = int(dihedral1.split()[0])
+    d1atom2_num = int(dihedral1.split()[1])
+    d1atom3_num = int(dihedral1.split()[2])
+    d1atom4_num = int(dihedral1.split()[3])
+
+    phi = np.linspace(start=0, stop=180, num=num_conform)
+    for P in phi:
+        dir_name = "{}/{}_{:.0f}deg/".format(os.getcwd(),molecule_name,P)
+        os.mkdir(dir_name)
+        file_name = "{}/{}_{:.0f}deg".format(dir_name,molecule_name,P)
+
+        SetDihedralDeg(molecule.GetConformer(),d1atom1_num, d1atom2_num, d1atom3_num, d1atom4_num, P)
+        with open(dir_name+"temp.xyz",'w+') as fout:
+            fout.write(str(MolToXYZBlock(molecule)))
+        mol = Molecule.from_file(dir_name+'temp.xyz')
+        os.remove(dir_name+'temp.xyz')
+        gau = GaussianInput(mol=mol,charge=0,spin_multiplicity=1,functional='uLC-wPBE',basis_set='cc-pVDZ',route_parameters={"iop(3/107={}, 3/108={})".format(w,w):"","opt":"modredundant"},link0_parameters={'%mem':'5GB','%nproc':'1','%chk':'{}.chk'.format('{}.chk'.format(file_name.split('/')[-1]))})
+        gjf_file = gau.write_file(dir_name+'temp.gjf')
+
+        with open(dir_name+'temp.gjf') as temp:
+            lines = temp.readlines()
+        os.remove(dir_name+'temp.gjf')
+        with open(file_name+'.gjf','w') as gjf:
+            gjf.writelines([item for item in lines[:-2]])
+            gjf.write("D * {} {} * F\n\n".format(d1atom2_num+1,d1atom3_num+1))
+
+    print("Torsion Gaussian input files for {} finsihed!".format(molecule_name))
+
+def RunJob(job_name,job):
+    """
+    Write runfile for and submit any SLURM job. job_name and job should each be a string.
+    """
+    runfile = os.path.join(os.getcwd(),"%s.job" %job_name)
+    with open(runfile,"w+") as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH -t 14-00:00:00\n")
+        fh.writelines("#SBATCH --job-name=%s\n" % job_name)
+        fh.writelines("#SBATCH -N 1\n")
+        fh.writelines("#SBATCH -n 32\n")
+        fh.writelines("#SBATCH -p SKY32M192_L\n")
+        fh.writelines("#SBATCH --account=col_cmri235_uksr\n")
+        fh.writelines("#SBATCH --mail-type=ALL\n")
+        fh.writelines("#SBATCH --mail-user=rdu230@uky.edu\n")
+        fh.writelines("#SBATCH --error=SLURM_JOB_%j.err\n")
+        fh.writelines("#SBATCH --output=SLURM_JOB_%j.out\n" )
+
+        fh.writelines("ulimit -u 32768\n")
+        fh.writelines("module load ccs/gaussian/g16-A.03/g16-sandybridge\n")
+        fh.writelines('echo "Job $SLURM_JOB_ID running on SLURM NODELIST: $SLURM_NODELIST "\n')
+        fh.writelines(". ~/.bashrc\n")
+        fh.writelines("source $(conda info --base)/etc/profile.d/conda.sh\n")
+        fh.writelines("conda activate myenv\n")
+
+        fh.writelines(job)
+
+    os.system("sbatch %s" %runfile)
+
+def MoveFiles(destination,starts_with="",ends_with=""):
+    """
+    Moves all files that start/end with a certain string to a new destination
+    """
+    for i in os.listdir(os.getcwd()):
+        if i.startswith(starts_with) and i.endswith(ends_with):
+            shutil.move(i,destination+i)
+
+def FuncAllFiles(func,starts_with="",ends_with=""):
+    """
+    Runs all files that start/end with a certain string through a function
+    """
+    for i in os.listdir(os.getcwd()):
+        if i.startswith(starts_with) and i.endswith(ends_with):
+            func(i)
+
+home = os.getcwd()
+
+os.chdir(home+"/wtuning")
+for i in os.listdir(os.getcwd()):
+    i_path = os.path.join(home,i)
+    if os.path.isdir(i_path):
+        os.chdir(i_path)
+        os.mkdir("{}/rotated_dihed/{}/".format(home,i))
+        try:
+            subprocess.call('cp *_opt_0.log output.log ../../rotated_dihed/{}'.format(i),shell=True)
+            os.chdir(home+"/rotated_dihed/")
+            MoveFiles("{}/rotated_dihed/{}/".format(home,i),starts_with=str(i[:-3]),ends_with='_dihed.txt')
+            normal = 1
+        except:
+            print("opuput.log or opt_0.log file not found for {}. Omega tuning optimization may not have completed!".format(i))
+            normal = 0
+
+        if normal == 1:
+            os.chdir("{}/rotated_dihed/{}/".format(home,i))
+            with open('output.log','r') as fn:
+                w_data = fn.readlines()[-2].split()[1]
+                w = "0{}".format(w_data.split('.')[1])
+            dihed_atoms_file = i+'_dihed.txt'
+            print(dihed_atoms_file)
+            MakesGauInputs('{}_opt_0.log'.format(i),dihed_atoms_file,w)
+            print("Gaussian input files made for {}!".format(i))
+        else:
+            continue
+        os.chdir(home+"/wtuning")
+
+rd = os.path.join(home,'rotated_dihed')
+os.chdir(rd)
+mols = os.listdir(rd)
+for m in mols:
+    mpath = os.path.join(rd,m)
+    if os.path.isdir(mpath):
+    	os.chdir(mpath)
+    	degs = os.listdir(mpath)
+    	for d in degs:
+            dpath = os.path.join(rd,m,d)
+            if os.path.isdir(dpath):
+                os.chdir(dpath)
+                RunJob('gau{}'.format(d[5:]),"g16 {}.gjf".format(d))
+                print("Gaussian jobs sumbitted for {}!".format(d))
+            else:
+                pass
+            os.chdir(mpath)
+    else:
+        pass
+    os.chdir(rd)
---
+++ find_TorsionEng_plts.py
@@ -0,0 +1,175 @@
+import os
+import re
+import shutil
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+def FindTorsionEnergies():
+	"""
+	Iterates through dihedral directory and finds energies for each torsion angle
+	for each molecule. It creates a energies directory with a energy cvs file
+	for each molecule.
+	"""
+	cwd = os.getcwd()
+	home = os.path.join(cwd,'rotated_dihed')
+	mols = os.listdir(home)
+	os.chdir(home)
+	for m in mols:
+		if os.path.isdir(m):
+			mpath = os.path.join(home,m)
+			os.chdir(mpath)
+			degs = os.listdir(mpath)
+			for d in degs:
+				if os.path.isdir(d):
+					dpath = os.path.join(home,m,d)
+					os.chdir(dpath)
+					try:
+						gen = [x for x in os.listdir(os.getcwd()) if x.endswith('.log')]
+						for log_file in gen:
+							with open(log_file) as fn:
+								for line in fn:
+									if re.search(r'SCF Done',line):
+										a = line.split()[4]
+						with open('{}/energies/energies_{}.cvs'.format(cwd,m),'a') as fout:
+							fout.write(d.split('_')[-1][:-3] + ',' + a + '\n')
+						print("Dihedral rotations energies collected for {}!".format(d))
+					except:
+						print(".log file not found for {}. Energy calculations for dihedral rotations did not finish!".format(d))
+						continue
+				os.chdir(mpath)
+		os.chdir(home)
+
+def SortEnergies():
+    """
+    In directory with all energie .cvs files, this copies all energies files into
+    a new "sorted_energies" directory. This contains directories for each unique
+    monomer base which each contains energies files for all polymer lengths of
+    this monomer base.
+    """
+    cwd = os.getcwd()
+    try:
+        os.mkdir(cwd+"/sorted_energies/")
+    except:
+        pass
+    list_attrib = []
+    #Recall naming  :  mols_<ring type>_<#repeat units>_<polymer#>_<subs>_<confromer#>
+    for file in os.listdir(os.getcwd()):
+        mol = str(file)
+        try:
+            ring_num = int(mol.split('_')[2])
+            unit_num = int(mol.split('_')[3])
+            polymer_num = int(mol.split('_')[4])
+            substituents = mol.split('_')[5]
+            list_attrib.append([ring_num,unit_num,polymer_num,substituents])
+        except:
+            pass
+    ring_type = [55,65,66]
+    unit_nums = [1,3,5,7]
+    for r in ring_type:
+        i=0
+        while i <= 100:
+            for j in list_attrib:
+                if j[0] == r and j[2] == i:
+                    mol_name = '{}/energies_mols_{}_{}_{}_{}*.cvs'.format(cwd,j[0],j[1],j[2],j[3])
+                    dir_name = '{}/sorted_energies/allenergies_mols_{}_{}_{}/'.format(cwd,j[0],j[2],j[3])
+                    try:
+                        os.mkdir(dir_name)
+                    except:
+                        pass
+                    subprocess.call('cp {} {}'.format(mol_name,dir_name),shell=True)
+            i+=1
+
+def MakeTorsionPlot(energy_file):
+	"""
+	Plots the energy vs the torsion angle for a molecule given a torsion energy
+	file. Saves the plot to a png file.
+	"""
+	mol_name = energy_file.strip('energies_ _0.cvs')
+	edata = np.genfromtxt(fname=energy_file,delimiter=',', dtype='unicode')
+	edata=edata.astype(np.float)
+
+	energy_values = edata[:,1]
+	phi = edata[:,0]
+
+	energy_range = max(energy_values) - min(energy_values)
+
+	plt.scatter(phi, energy_values, color='MediumVioletRed')
+	plt.xlim(min(phi)-3, max(phi)+3)
+	plt.xticks(np.linspace(start=0, stop=180, num=7))
+	plt.ylim(top = max(energy_values) + energy_range*0.15,bottom = min(energy_values) - energy_range*0.15)
+	plt.yticks(np.linspace(start=min(energy_values), stop=max(energy_values), num=5))
+	plt.xlabel("dihedral angle in degrees")
+	plt.ylabel("energy (Hartrees)")
+	plt.title("{} torsion energy".format(mol_name))
+	plt.savefig('torsionE_plt_{}.png'.format(mol_name),dpi=300)
+
+def OverlayPlts():
+    """
+    Makes overlay plot of allenergies files in cwd.
+    """
+    cwd = os.getcwd()
+    mol_name = cwd.split('/')[-1].strip('allenergies_')
+    plt.figure()
+    gen = [x for x in os.listdir(os.getcwd()) if x.endswith('.cvs')]
+    for energy_file in gen:
+        poly_len = str(energy_file).split('_')[3]
+        edata = np.genfromtxt(fname=energy_file,delimiter=',', dtype='unicode')
+        edata=edata.astype(np.float)
+
+        i=0
+        while i < edata.shape[0]:
+            if edata[i,0] == 0:
+                zero_energy = edata[i,1]
+            i+=1
+
+        energy_values = edata[:,1]
+        phi = edata[:,0]
+
+        plt.scatter(phi, energy_values-zero_energy, label=poly_len)
+
+    plt.xlim(-3, 183)
+    plt.xticks(np.linspace(start=0, stop=180, num=7))
+    plt.xlabel("dihedral angle in degrees")
+    plt.ylabel("energy (Hartrees)")
+    plt.title("{} torsion energy".format(mol_name))
+    plt.legend()
+    plt.savefig('overlay_plt_{}.png'.format(mol_name),dpi=300)
+
+def MoveFiles(destination,starts_with="",ends_with=""):
+	"""
+	Moves all files that start/end with a certain string to a new destination
+	"""
+	for i in os.listdir(os.getcwd()):
+		if i.startswith(starts_with) and i.endswith(ends_with):
+			shutil.move(i,destination+i)
+
+cwd = os.getcwd()
+FindTorsionEnergies()
+print("Done gathering torsion energies!")
+
+os.chdir(cwd+'/energies')
+for f in os.listdir(os.getcwd()):
+	if f.startswith("energies"):
+		try:
+			MakeTorsionPlot(f)
+		except:
+			pass
+os.mkdir('plots')
+MoveFiles(os.path.join(cwd,'energies','plots'),ends_with='.png')
+
+os.chdir(cwd)
+
+SortEnergies()
+os.mkdir(cwd+"/overlay_plts")
+os.chdir(cwd+"/sorted_energies/")
+for mol in os.listdir(os.getcwd()):
+    if mol.startswith("allenergies_"):
+        print(mol)
+        os.chdir("{}/sorted_energies/{}".format(cwd,mol))
+        OverlayPlts()
+        MoveFiles(cwd+"/overlay_plts/",ends_with='.png')
+    else:
+        pass
+print("Done makeing overlay plots!")
